fbf30a80783839b0db3b04af21966bc38d2f5d79
wycemiro <wycemiro2@gmail.com>
2021-11-10 10:05:41 +0300

    SearchAvailabilityByDates

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,13 @@
 package repository
 
 import (
+	"time"
+
 	"github.com/wycemiro/booking-site/internal/models"
 )
 
 type DatabaseRepo interface {
 	InsertReservation(res models.Reservation) (int, error)
 	InstertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
