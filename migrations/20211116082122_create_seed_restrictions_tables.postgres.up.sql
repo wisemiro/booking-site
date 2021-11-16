f992f8d91fd84cd14d49d47197629392624bf68e
wycemiro <wycemiro2@gmail.com>
2021-11-16 11:26:39 +0300

    restrictions sql db seed

---
+++ migrations/20211116082122_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO PUBLIC.restrictions (restriction_name,created_at,updated_at)VALUES
+('Reservation','2021-11-11 00:00:00.000','2021-11-11 00:00:00.000'),
+('Owner Block','2021-11-11 00:00:00.000','2021-11-11 00:00:00.000');
